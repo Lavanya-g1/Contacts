34fba76bec7d72a2b5f22cbdaf94bf051d6f4ab9
Lavanya-g1 <113387191+Lavanya-g1@users.noreply.github.com>
2022-09-13 14:08:55 -0500

    Adding Ssn variable to Person Object

--- Person.java
+++ Person.java
@@ -14,7 +14,8 @@ this.Ssn = Ssn;
     public static void main(String[] args) {
         Person p = new Person("Lavanya",11,"XXXXX0000");
         System.out.println(p.Name);
-        System.out.println(p.Age);
+        System.out.println(p.Age);  
+        System.out.println(p.Ssn);
       
         
        
