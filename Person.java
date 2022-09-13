32e14a5c3179386916e7651c8d1e0b74f7275312
Lavanya-g1 <113387191+Lavanya-g1@users.noreply.github.com>
2022-09-13 13:13:26 -0500

    Contacts: Adding Name Variable

---
+++ Person.java
@@ -0,0 +1,18 @@
+class Person{
+    String Name;
+    int Age;
+    String Ssn;
+
+
+    public static void main(String[] args) {
+        Person p = new Person();
+        p.Name= "Lavanya";
+     
+        System.out.println(p.Name);
+        
+
+    }
+
+
+}
+
